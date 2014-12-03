363be8ca863b2c937131f01664e199dea0539d80
Remo Regli <remo.regli@gmail.com>
2014-12-03 15:13:53 +0100

    exported do/while loops to method get_answer, added check for 0 division

--- calculator_cmd_ln.rb
+++ calculator_cmd_ln.rb
@@ -1,5 +1,6 @@
 # calculator for command line
 
+
 def say (msg)
   puts "=> #{msg}"
 end
@@ -8,45 +9,58 @@ def input_valid? (input, regexp)
   input =~ regexp ? true : false
 end
 
+def get_answer (question, valid_input, &block)
+  begin
+    say question
+    input = block.call
+  end until input_valid? input, valid_input
+
+  return input
+end
+
 puts "*** calculator started ***"
 
-restart = ''
+restart = 'y'
 
 begin
 
-  begin
-    say "Enter first number:"
-    num1 = gets.chomp
-  end until input_valid? num1, /[0-9]/
+  num1 = get_answer "Enter first number:", /[0-9]/ do
+    gets.chomp
+  end
 
-  begin
-    say "Enter second number:"
-    num2 = gets.chomp
-  end until input_valid? num2, /[0-9]/
+  num2 = get_answer "Enter second number:", /[0-9]/ do
+    gets.chomp
+  end
 
-  begin
-    say "Select operation:"
+  operator = get_answer "Select operation:", /^1$|^2$|^3$|^4$/ do
     say "[1] add, [2] subtract, [3] multiply, [4] divide"
-    operator = gets.chomp
-  end until input_valid? operator, /^1$|^2$|^3$|^4$/
-
+    gets.chomp
+  end
 
   result = case operator.to_i
     when 1 then num1.to_i + num2.to_i
     when 2 then num1.to_i - num2.to_i
     when 3 then num1.to_i * num2.to_i
-    when 4 then num1.to_f / num2.to_f
+    when 4 then if num2.to_f == 0
+                  say "division by 0 not possible!"
+                else
+                  num1.to_f / num2.to_f
+                end
     else "operator not valid!"
   end
 
-  say "result: #{result}"
+  #print out result and make some space 
+  say "result: #{result}\n\n\n\n"
 
-  begin
-    say "Do you want to make another calculation? [Y/n]"
-    restart = gets.chomp.downcase
+  restart = get_answer "Do you want to make another calculation? [Y/n]", /^y$|^n$/ do
+    input = gets.chomp.downcase
     #"Y" is default, so check if user hits enter
-    restart = "y" if restart == ""
-  end until input_valid? restart, /^y$|^n$/
+    if input == ""
+      answer = "y"
+    else
+      answer = input
+    end
+  end
 
 end while restart == "y"
 
