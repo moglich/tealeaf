cbf384a3343dd52c1dace56ad7967632c4a9a8cb
Remo Regli <remo.regli@gmail.com>
2014-12-19 14:33:21 +0100

    removed inheritance for class Player from Board, removed class var for grid

--- Lesson2/tic_tac_toe_OO.rb
+++ Lesson2/tic_tac_toe_OO.rb
@@ -1,9 +1,11 @@
 #Tic Tac Toe game, OO version
 
 class Board
+  attr_accessor :grid
+
   def initialize
-    @@grid = {}
-    (1..9).each { |pos| @@grid[pos] = ' ' }
+    @grid = {}
+    (1..9).each { |pos| @grid[pos] = ' ' }
   end
 
   def show
@@ -11,29 +13,25 @@ class Board
     puts "----  Tic Tac Toe  ----"
     puts
     puts "       |       |"
-    puts "   #{@@grid[1]}   |   #{@@grid[2]}   |   #{@@grid[3]}"
+    puts "   #{@grid[1]}   |   #{@grid[2]}   |   #{@grid[3]}"
     puts "       |       |"
     puts "-------+-------+-------"
     puts "       |       |"
-    puts "   #{@@grid[4]}   |   #{@@grid[5]}   |   #{@@grid[6]}"
+    puts "   #{@grid[4]}   |   #{@grid[5]}   |   #{@grid[6]}"
     puts "       |       |"
     puts "-------+-------+-------"
     puts "       |       |"
-    puts "   #{@@grid[7]}   |   #{@@grid[8]}   |   #{@@grid[9]}"
+    puts "   #{@grid[7]}   |   #{@grid[8]}   |   #{@grid[9]}"
     puts "       |       |"
     puts ""
   end
 
   def get_empty_positions
-    @@grid.keys.select {|position| @@grid[position] == ' '}
-  end
-
-  def grid
-    @@grid
+    @grid.keys.select {|position| @grid[position] == ' '}
   end
 end
 
-class Player < Board
+class Player
   attr_reader :name, :mark, :player_type
 
   def initialize(name, mark, type = "computer")
@@ -42,17 +40,17 @@ class Player < Board
     @player_type = type
   end
 
-  def pick_field
-    unless get_empty_positions.empty?
+  def pick_field(board)
+    unless board.get_empty_positions.empty?
       if self.player_type == "computer"
-        field = get_empty_positions.sample
+        field = board.get_empty_positions.sample
       else
         begin
           puts "Pick field #{self.name}:"
           field = gets.chomp.to_i
-        end until @@grid[field] == ' '
+        end until board.grid[field] == ' '
       end
-      @@grid[field] = self.mark
+      board.grid[field] = self.mark
     else
       puts "It's a tie - no fields left!"
       exit
@@ -63,7 +61,7 @@ end
 class Game
   def initialize
     @board = Board.new
-    
+
     puts "What is your name?"
     username = gets.chomp
     @player = Player.new(username, 'X', "user")
@@ -73,8 +71,8 @@ class Game
   def play
     begin
       @board.show
-      @player.pick_field
-      @computer.pick_field
+      @player.pick_field(@board)
+      @computer.pick_field(@board)
       @board.show
     end until winner?
     puts "Winner is: #{winner?}"
